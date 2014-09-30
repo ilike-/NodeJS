bb4cb7a102dcaf965a12ca668fc62e38a334bb02
ilike <wtliu001@gmail.com>
2014-10-01 00:54:47 +0800

    run

--- readme.js
+++ readme.js
@@ -7,4 +7,4 @@ http.createServer( function (req, res){
 	res.writeHead(200, {'Content-Type':'text/plain'});
 	res.end('Welcome to the world of Node.js');
 }).listen(8088, '127.0.0.1');
- console.log("Server running at Http://127.0.0.1");
+ console.log("Server running at Http://127.0.0.1:8088");
